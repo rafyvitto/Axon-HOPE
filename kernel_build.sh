4a827a7cb6f0a5be17e613d22586f074de728c0d
rafyvitto <rafymercenario@hotmail.com>
2020-10-18 13:55:01 -0400

    Added Kernel build start/end to build script and started version naming,taken from https://github.com/pappschlumpf/SmurfKernelOP7/blob/970b0378632c3ef5437f3a7ed1fc9529a8467cb8/build_gcc9.sh

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
 
 rm -rf out
 mkdir out
@@ -9,8 +14,33 @@ export DTC_EXT=dtc
 make O=out ARCH=arm64 pine_defconfig
 
 PATH="${PWD}/bin:${PWD}/toolchain/bin:${PATH}" \
+
+DATE_START=$(date +"%s")
+
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making Kernel:"
+echo "-----------------"
+echo -e "${restore}" 
+
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
                       CROSS_COMPILE=aarch64-linux-android- | tee kernel.log
+                      
+# Kernel Details
+VER=".1.0.0.unified"  
+
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo                   
